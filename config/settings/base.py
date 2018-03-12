2bfbd4481345944929a402dd108ab7848e43a472
thefonso <thefonso@thefonso.com>
2018-03-11 20:23:47 -0500

    god who knows what this is

--- config/settings/base.py
+++ config/settings/base.py
@@ -112,7 +112,7 @@ MIDDLEWARE = [
 ]
 
 # For allowing external Vuejs client to connect
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     'localhost:8080',
