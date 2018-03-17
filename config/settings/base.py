d8ef472cada135aad5213d96c8ec5ccbe8ad14b5
thefonso <thefonso@thefonso.com>
2018-03-17 11:12:38 -0500

    added CORS back so login works on KT front-end. Note backend admin login works fine as well

--- config/settings/base.py
+++ config/settings/base.py
@@ -58,7 +58,7 @@ REST_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
-   # 'corsheaders',
+    'corsheaders',
 
 ]
 
@@ -96,12 +96,20 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# For allowing external Vuejs client to connect
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080',
+)
 
 # MIGRATIONS CONFIGURATION
 # ------------------------------------------------------------------------------
