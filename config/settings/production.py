adfd2832048e1bfd89a36618e1cecc18bdff7bbb
thefonso <thefonso@thefonso.com>
2018-05-08 21:52:23 -0500

    added comment so we know what to turn back on for mailgun

--- config/settings/production.py
+++ config/settings/production.py
@@ -115,6 +115,7 @@ EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[KittyTracker
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
 
 # Anymail with Mailgun
+# FONSO turned this off for now
 # INSTALLED_APPS += ['anymail', ]
 # ANYMAIL = {
 #     'MAILGUN_API_KEY': env('DJANGO_MAILGUN_API_KEY'),
