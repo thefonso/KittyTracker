7c0350db4db7aa0fe9077bc5688c286c101eadd9
thefonso <thefonso@thefonso.com>
2018-08-19 13:21:16 -0500

    graphiql debug working now via http://docs.graphene-python.org/projects/django/en/latest/debug/

--- kittytracker/schema.py
+++ kittytracker/schema.py
@@ -1,11 +1,14 @@
 import graphene
 
 import kittytracker.tracker.schema
+from graphene_django.debug import DjangoDebug
 
 
 class Query(kittytracker.tracker.schema.Query, graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
+    debug = graphene.Field(DjangoDebug, name='__debug')
     pass
+
 
 schema = graphene.Schema(query=Query)
